63b5ae493bff34ca4cf80a4234be28b2769e5520
Md Noor Ahmed <mdnoorahmed@Mac.mynet>
2025-05-05 23:33:28 +0100

    feat: add email OTP system for account verification

--- Desktop/mern-auth/server/controllers/authController.js
+++ Desktop/mern-auth/server/controllers/authController.js
@@ -100,3 +100,37 @@ export const logout = async (req, res)=> {
         return res.json({ success: false, message: error.message});
     }
 }
+
+//Send verification otp to the users email
+export const sendVerifyOtp = async(req, res)=>{
+    try {
+        const {userId} = req.body;
+
+        const user = await userModel.findById(userId);
+
+        if(user.isAccountVerified) {
+            return res.json({success: false, message: "Account already verified"})
+        }
+
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.verifyOtp = otp;
+    user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000
+
+    await user.save();
+
+    const mailOption = {
+        from: process.env.SENDER_EMAIL,
+        to: user.email,
+        subject: 'Account Verification OTP',
+        text: `Your OTP is ${otp}. Verify your account using this OTP.`
+    }
+
+    await transporter.sendMail(mailOption);
+
+    res.json({ success: true, message: 'Verification OTP Sent on Email'});
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message});
+    }
+}
