a44f3df5e608b91d0c9213cfa896a8b3ad1c62d5
Md Noor Ahmed <mdnoorahmed@Mac.mynet>
2025-05-06 00:29:10 +0100

    added password reset otp and auth check

--- Desktop/mern-auth/server/controllers/authController.js
+++ Desktop/mern-auth/server/controllers/authController.js
@@ -135,6 +135,7 @@ export const sendVerifyOtp = async(req, res)=>{
     }
 }
 
+// Verify the email using the otp
 export const verifyEmail = async (req, res) =>{
     const {userId, otp} = req.body;
 
@@ -167,5 +168,52 @@ export const verifyEmail = async (req, res) =>{
         return res.json({ success: false, message: error.message});
 
     }
+}
 
+// Check if user is authenticated
+export const isAuthenticated = async (req, res) => { 
+    try {
+        return res.json({ success: true});      
+    } catch (error) {
+         res.json({ success: false, message: error.message});
+    }
+}
+
+// Send password reset otp 
+
+export const sendResetOtp = async (req, res)=>{
+    const {email} = req.body;
+
+    if(!email) { 
+        return res.json({success: false, message: 'Email is required'})
+    }
+
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+         return res.json({success: false, message: 'User not found'});
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.resetOtp = otp;
+    user.resetOtpExpireAt = Date.now() + 15 * 60 * 1000
+
+    await user.save();
+
+    const mailOption = {
+        from: process.env.SENDER_EMAIL,
+        to: user.email,
+        subject: 'Password Reset OTP',
+        text: `Your OTP for resetting your password is ${otp}.
+        Use this OTP to proceed with resetting your password.`
+    };
+
+    await transporter.sendMail(mailOption);
+
+    return res.json({success: true, message: 'OTP sent to your email'});
+        
+    } catch (error) {
+       return res.json({ success: false, message: error.message});
+    }
 }
