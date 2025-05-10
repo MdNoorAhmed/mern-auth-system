fadd988ed1f459d8ecf099f738b259fb476444cd
Md Noor Ahmed <mdnoorahmed@Mac.mynet>
2025-05-10 14:30:05 +0100

    Implement full reset password flow with email, OTP, and password update

--- Desktop/mern-auth/client/src/pages/ResetPassword.jsx
+++ Desktop/mern-auth/client/src/pages/ResetPassword.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const ResetPassword = () => {
+
+  const {backendurl} = useContext(AppContext)
+  axios.defaults.withCredentials = true
 
   const navigate = useNavigate()
   const  [email, setEmail] = useState('')
@@ -39,6 +45,38 @@ const ResetPassword = () => {
         })
     }
 
+    const onSubmitEmail = async (e)=>{
+      e.preventDefault();
+      try {
+        const {data} = await axios.post(backendurl + '/api/auth/send-reset-otp',
+        {email})
+        data.success ? toast.success(data.message) : toast.error(data.message)
+        data.success && setIsEmailSent(true)
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
+
+    const onSubmitOTP = async(e) =>{
+         e.preventDefault();
+         const otpArray = inputRefs.current.map(e => e.value)
+          setOtp(otpArray.join(''))
+          setIsOtpSubmitted(true)
+    }
+
+    const onSubmitNewPassword = async(e) => {
+        e.preventDefault();
+        try {
+          const {data} = await axios.post(backendurl + '/api/auth/reset-password',
+            {email, otp, newPassword} )
+            data.success ? toast.success(data.message) : toast.error(data.message)
+             data.success && navigate('/login')
+         
+        } catch (error) {
+          toast.error(data.message)
+        }
+    }
+
   return (
     <div className='flex items-center justify-center min-h-screen
  bg-gradient-to-br from-blue-200 to-purple-400'>
@@ -49,7 +87,7 @@ const ResetPassword = () => {
 {/* enter email id */}
 {!isEmailSent && 
 
-    <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+    <form onSubmit={onSubmitEmail} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
 
      <h1 className='text-white text-2xl font-semibold text-center mb-4'>Reset password</h1>
       <p className='text-center mb-6 text-indigo-300'>Enter your registered email address</p>
@@ -72,7 +110,7 @@ const ResetPassword = () => {
 
 {!isOtpSubmitted && isEmailSent && 
  
-  <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+  <form onSubmit={onSubmitOTP} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
             <h1 className='text-white text-2xl font-semibold text-center mb-4'>Reset password OTP</h1>
             <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email id.</p>
             <div className='flex justify-between mb-8' onPaste={handlePaste}>
@@ -98,7 +136,7 @@ const ResetPassword = () => {
 
 {isOtpSubmitted && isEmailSent && 
 
-<form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+<form onSubmit={onSubmitNewPassword} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
 
      <h1 className='text-white text-2xl font-semibold text-center mb-4'>New password</h1>
       <p className='text-center mb-6 text-indigo-300'>Enter the new password below</p>
